3d97d2b7de60cbf1f5b9465fd64f4cdff13a8f15
Anar <ai.anarul4232@gmail.com>
2025-01-19 13:12:13 +0600

    hello world project modify

--- hello-world-project/src/app/page.tsx
+++ hello-world-project/src/app/page.tsx
@@ -1,4 +1,7 @@
-
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h3 className="text-3xl font-semibold text-green-600">Hello World</h3>
+    </div>
+  );
 }
